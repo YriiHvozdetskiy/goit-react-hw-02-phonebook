a4001941f77e40519bccb8021af1de089e7aef01
YriiHvozdetskiy <hvozdetskiyyrii@gmail.com>
2021-07-14 16:17:09 +0300

    test 2

--- src/App.js
+++ src/App.js
@@ -2,12 +2,8 @@ import { Form } from './components/Input/Form';
 
 export const App = () => {
   return (
-    <div>
+    <>
       <Form />
-    </div>
+    </>
   );
-};
-
-export const App = () => {
-  return <></>;
 };
